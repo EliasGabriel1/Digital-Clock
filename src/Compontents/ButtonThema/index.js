9aa31b8bf93248fd72cf217bfb53421454a9ecfe
EliasGabriel1 <elias.147682@hotmail.com>
2021-11-20 17:40:28 -0300

    update

--- src/Compontents/ButtonThema/index.js
+++ src/Compontents/ButtonThema/index.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 
-export default class ButtonTheme extends Component {
+export default class ButtonTheme extends Component {    
     render() {
+        let toggle = () =>{
+            let body = document.querySelector("body")
+            body.classList.toggle("dark-theme")
+        }
         return (
-            <main>
-        <div class="liga-desliga">
-            <input type="checkbox" class="liga-desliga__checkbox" id="liga-desliga"/>
-            <label for="liga-desliga" class="liga-desliga__botao"></label>
-        </div>
-    </main>
+        <main>
+            <div className="liga-desliga">
+                <input onClick={toggle} type="checkbox" className="liga-desliga__checkbox" id="liga-desliga"/>
+                <label htmlFor="liga-desliga" className="liga-desliga__botao"></label>
+            </div>
+        </main>
         )
     }
 }
